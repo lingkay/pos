6bdcec382b9d746132cda5ef05d4e4a5cdc88032
Richard (5559) <richard.umayan@icloud.com>
2017-07-25 23:28:27 +0000

    added individual item adjustments

--- src/Gist/POSBundle/Controller/POSController.php
+++ src/Gist/POSBundle/Controller/POSController.php
@@ -12,7 +12,19 @@ class POSController extends Controller
     	$this->title = 'Dashboard';
         $params = $this->getViewParams('', 'gist_dashboard_index');
 
-        
+        $params['indiv_options'] = array(
+            'none' => 'None',
+            'gift' => 'Gift/Free',
+            '40p' => '40% Discount',
+            'chg' => 'Change of Price'
+        );
+
+        $params['bulk_options'] = array(
+            '1' => 'Gift',
+            '2' => 'Discount Amount',
+            '3' => 'Discount',
+            '4' => 'Amount to Pay'
+        );
 
         return $this->render('GistPOSBundle:Dashboard:index.html.twig', $params);
     }
