ec832d52aa2522dbbcdd93cf13b5eea10a102185
DESKTOP-U8MIKS1\richa <richard.umayan@icloud.com>
2018-01-16 21:10:53 +0800

    updates

--- src/Gist/UserBundle/Model/UserManager.php
+++ src/Gist/UserBundle/Model/UserManager.php
@@ -64,6 +64,17 @@ class UserManager
         );
     }
 
+    public function getUserFullNameOptions($filter = array())
+    {
+        return $this->getOptionsArray(
+            'GistUserBundle:User',
+            $filter,
+            array('username' => 'ASC'),
+            'getID',
+            'getDisplayName'
+        );
+    }
+
     public function getGroupOptions($filter = array())
     {
         return $this->getOptionsArray(
